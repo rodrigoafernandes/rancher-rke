bd5cecb6e74dfc8a0d4b26c72884df1b0a9473b2
Rodrigo Fernandes <rodrigoforit@gmail.com>
2023-12-03 19:23:48 -0300

    fist commit

---
+++ provisionamento/init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+validateCommand() {
+  if [ $? -eq 0 ]; then
+    echo "[OK] $1"
+  else
+    echo "[ERROR] $1"
+    exit 1
+  fi
+}
+
+# Inserindo chave SSH
+sudo test -f /root/.ssh/id_rsa
+if [ $? -eq 1 ]; then
+  sudo mkdir -p /root/.ssh/ && \
+	  sudo cp /tmp/devsecops.pem /root/.ssh/id_rsa && \
+	  sudo cp /tmp/devsecops.pub /root/.ssh/authorized_keys && \
+	  sudo chmod 600 /root/.ssh/id_rsa
+  
+  validateCommand "Preparando SSH KEY"
+else
+  echo "[OK] SSH KEY"
+fi
